7fae9192b56d63250e972bf8aba7ca3f9c7d552f
Igor Lylko <and>
2021-03-15 18:04:23 +0300

    Version 1.0.1

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ dp = Dispatcher(bot)
 #===============================================================================
 db = MySQL()
 
-
 #-----------------------------изменение статуса---------------------------------
 @dp.message_handler(commands=['change_status', 'status'])
 async def status(message: types.Message):
@@ -42,6 +41,36 @@ async def setcommand(message: types.Message):
 			db.send_command(message.from_user.id, 1)
 			db.commit()
 			await message.answer("Команда принята и будет исполнена через 30 секунд. Сообщение о исполнении придет на почту.")
+
+@dp.message_handler(commands=['help'])
+async def help(message: types.Message):
+	if(db.user_exist(message.from_user.id) == False):
+		await message.answer("Вас приветствует бот для управления и просмотра информации о состоянии вашего рига. \nВ базе нет данных о Вас. Чтобы создать нового пользователя воспользуйтесь командой /new. \n Чтобы просмотреть все команды воспользуйтесь командой /commands.")
+	else:
+		await message.answer("Вас приветствует бот для управления и просмотра информации о состоянии вашего рига. \nЧтобы просмотреть все возможные команды воспользуйтесь командой /commands")
+
+@dp.message_handler(commands=['commands'])
+async def available_commands(message: types.Message):
+	if(db.user_exist(message.from_user.id) == False):
+		await message.answer("В базе нет данных о Вас. Чтобы создать нового пользователя воспользуйтесь командой /new.\n/show - показывает все установленные данные (Включен/Выключен контроль, выполнены ли все команды, ваш id)\n/status - включает контроль, если он выключен и наоборот.\n/command=? - отдача команды для рига. Вместо знака (?) поставить нужный индекс.")
+	else:
+		await message.answer("/show - показывает все установленные данные (Включен/Выключен контроль, выполнены ли все команды, ваш id)\n/status - включает контроль, если он выключен и наоборот.\n/command=? - отдача команды для рига. Вместо знака (?) поставить нужный индекс.")
+		
+@dp.message_handler(commands=['new'])
+async def new_user(message: types.Message):
+        if (db.user_exist(message.from_user.id) == True):
+            await message.answer("Ваш аккаунт уже зарегистрирован")
+        else:
+            db.add_subscriber(message.from_user.id)
+            await message.answer("Создан новый пользователь.")
+
+@dp.message_handler(commands=['show'])
+async def show_info(message: types.Message):
+        if (db.user_exist(message.from_user.id) == True):
+            await message.answer("Ваш личный id (его надо будет указать в программе рига): {}.\nСтатус пользования: {}. (1 - контроль включен,0 - выключен. Чтобы изменить это значение - /status).\nСтатус выполнения команды: {}. (0 - все команды выполнены, любое другое значение - команда ожидает выполнения.)".format(db.check_parametrs(message.from_user.id)[0][1],db.check_parametrs(message.from_user.id)[0][2],db.check_parametrs(message.from_user.id)[0][3]))
+        else:
+            await message.answer("Сначала создайте нового пользователя /new")
+
 
 
 #-------
